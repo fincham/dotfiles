633d5671cad9c41e0169a797f1f81de4dc12dd6c
Michael Fincham <michael@hotplate.co.nz>
2020-11-01 17:17:15 +1300

    Allow for OpenBSD

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 """
 Settings for fincham-dotfiles, mostly used by the templating engine (so it is expected that
@@ -8,19 +8,24 @@ Michael Fincham <michael@hotplate.co.nz>
 """
 
 import os
+import platform
 import socket
 
 hostname = socket.gethostname()
 run_dir = os.environ.get('XDG_RUNTIME_DIR', '/run/user/%i' % os.getuid())
 
 # if the machine has a battery, it's probably at least a bit portable. this is used later for "laptop specific" stuff.
-batteries = [
-    entry for entry in os.listdir("/sys/class/power_supply") if entry.startswith("BAT")
-]
+if platform.system() == "Linux":
+    batteries = [
+        entry for entry in os.listdir("/sys/class/power_supply") if entry.startswith("BAT")
+    ]
+    # look for wireless interfaces to configure i3status etc
+    wifi_interfaces = [
+        interface.split("/")[-1]
+        for interface in os.listdir("/sys/class/net")
+        if "phy80211" in os.listdir("/sys/class/net/%s" % interface)
+    ]
+else:
+    batteries = []
+    wifi_interfaces = []
 
-# look for wireless interfaces to configure i3status etc
-wifi_interfaces = [
-    interface.split("/")[-1]
-    for interface in os.listdir("/sys/class/net")
-    if "phy80211" in os.listdir("/sys/class/net/%s" % interface)
-]
